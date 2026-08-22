0e493eb7efcadf134d385ee357d43d6b872e8c2f
hamyunguu <hcy070722@gmail.com>
2026-08-22 13:41:52 +0900

    서비스 주문서 필드와 재단선을 피그마 좌표에 맞춤

--- hero.js
+++ hero.js
@@ -27,9 +27,9 @@ if (loader) {
 /* ---------------- left: the poster the numbering pages through ---------------- */
 
 const POSTERS = [
-  'assets/fig-poster.jpg?v=66',
-  'assets/card-hand.jpg?v=66',
-  'assets/gal-wood-a.jpg?v=66',
+  'assets/fig-poster.jpg?v=69',
+  'assets/card-hand.jpg?v=69',
+  'assets/gal-wood-a.jpg?v=69',
 ];
 
 const layers = [...document.querySelectorAll('.poster img')];
