288de746148d8b46383ed88bee39b7098f6159bc
hamyunguu <hcy070722@gmail.com>
2026-09-03 13:39:49 +0900

    아카이브 작품별 인쇄 사양 팝업을 추가해 페이지 안에서 확인

--- hero.js
+++ hero.js
@@ -27,9 +27,9 @@ if (loader) {
 /* ---------------- left: the poster the numbering pages through ---------------- */
 
 const POSTERS = [
-  'assets/fig-poster.jpg?v=85',
-  'assets/card-hand.jpg?v=85',
-  'assets/gal-wood-a.jpg?v=85',
+  'assets/fig-poster.jpg?v=87',
+  'assets/card-hand.jpg?v=87',
+  'assets/gal-wood-a.jpg?v=87',
 ];
 
 const layers = [...document.querySelectorAll('.poster img')];
