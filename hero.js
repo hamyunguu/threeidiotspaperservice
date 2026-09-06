e07e95d0a3ffe901168be6b1eb546a0db3cce403
hamyunguu <hcy070722@gmail.com>
2026-09-06 18:50:25 +0900

    선택한 판형의 검정 상태 유지

--- hero.js
+++ hero.js
@@ -27,9 +27,9 @@ if (loader) {
 /* ---------------- left: the poster the numbering pages through ---------------- */
 
 const POSTERS = [
-  'assets/fig-poster.jpg?v=97',
-  'assets/card-hand.jpg?v=97',
-  'assets/gal-wood-a.jpg?v=97',
+  'assets/fig-poster.jpg?v=98',
+  'assets/card-hand.jpg?v=98',
+  'assets/gal-wood-a.jpg?v=98',
 ];
 
 const layers = [...document.querySelectorAll('.poster img')];
